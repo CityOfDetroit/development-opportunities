a72f4c90db377682e464f1b07496a304ce9a960f
Edgar Montes <jedgar1mx@gmail.com>
2021-01-04 14:05:17 -0500

    Adding image version switch, fix streetview position and bearing.

--- src/components/Streetview.js
+++ src/components/Streetview.js
@@ -25,98 +25,12 @@ export default class Streetview {
         this.createStreetView(this);
     }
 
-    wrap(value, min, max) {
-        let interval = max - min;
-      
-        while (value > max || value < min) {
-          if (value > max) {
-            value = value - interval;
-          } else if (value < min) {
-            value = value + interval;
-          }
-        }
-      
-        return value;
-    }
-
-    loadImagery(_streetview){ 
-        _streetview.setLoading(_streetview, false);
-        _streetview.mly = new Mapillary.Viewer({
-            // Replace this with your own client ID from mapillary.com
-            apiClient: _streetview.mapillaryClientID,
-            component: {
-                cover: false,
-                marker: true,
-                sequence: false,
-                cache: true,
-                attribution: false,
-            },
-            container: 'mly',
-        });
-
-        _streetview.mly.on("nodechanged", (n) => {
-            _streetview.svCoords = n.latLon;
-        });
-        
-        _streetview.mly.on("bearingchanged", (b) => {
-            _streetview.svBearing = b;
-        });
-
-        _streetview.defaultMarker = new Mapillary.MarkerComponent.SimpleMarker("default-id", { lat: _streetview.app.coords[1], lon: _streetview.app.coords[0] }, _streetview.markerStyle);
-        _streetview.markerComponent = _streetview.mly.getComponent("marker");
-        _streetview.markerComponent.add([_streetview.defaultMarker]);
-
-        _streetview.mly.moveToKey(_streetview.app.currentImageKey.properties.key).then((node) => {
-            _streetview.setBearing(_streetview, node, _streetview.mly, _streetview.app.currentImageKey.geometry.coordinates, [_streetview.app.coords[0], _streetview.app.coords[1]]);
-        });
-
-        _streetview.app.map.map.loadImage(videoIcon, (error, image) => {
-            if (error) throw error;
-            if(!_streetview.app.map.map.hasImage('video')){
-                _streetview.app.map.map.addImage('video', image);
-            }
-            _streetview.app.map.map.getSource('mapillary').setData({
-              type: "FeatureCollection",
-              // we'll make the map data here
-              features: [
-                {
-                  type: "Feature",
-                  geometry: {
-                    type: "Point",
-                    coordinates: [_streetview.app.coords[0], _streetview.app.coords[1]],
-                  },
-                  properties: {
-                    bearing: _streetview.app.svBearing - 90,
-                  },
-                },
-              ],
-            });
-        }); 
-    }
-
-    setLoading(_streetview, status){
-        if(status){
-            document.querySelector('.streetview-loader').className = 'streetview-loader active';
-            _streetview.loading = true;
-        }else{
-            document.querySelector('.streetview-loader.active').className = 'streetview-loader';
-            _streetview.loading = false;
-        }
-    }
-
-    changingImageDate(ev, _streetview){
-        console.log('changing date for imagery');
-        console.log(ev);
-    }
-
-
-
     createStreetView(_streetview){
         console.log('building components');
         // creating streetview components
         let header          = document.createElement('article');
         let title           = document.createElement('div');
-        let date         = document.createElement('div');
+        let date            = document.createElement('div');
         let dateLabel       = document.createElement('label');
         let dateSelect      = document.createElement('select'); 
         let imagery         = document.createElement('section');
@@ -169,38 +83,87 @@ export default class Streetview {
         _streetview.getImageKey(_streetview);
     }
 
-    bearingToBasic(_streetview, desiredBearing, nodeBearing) {
-        // 1. Take difference of desired bearing and node bearing in degrees.
-        // 2. Scale to basic coordinates.
-        // 3. Add 0.5 because node bearing corresponds to the center
-        //    of the image. See
-        //    https://mapillary.github.io/mapillary-js/classes/viewer.html
-        //    for explanation of the basic coordinate system of an image.
-        var basic = (desiredBearing - nodeBearing) / 360 + 0.5;
-      
-        // Wrap to a valid basic coordinate (on the [0, 1] interval).
-        // Needed when difference between desired bearing and node
-        // bearing is more than 180 degrees.
-        return _streetview.wrap(basic, 0, 1);
+    setCameraIcon(_streetview){
+        _streetview.app.map.map.getSource('mapillary').setData({
+            type: "FeatureCollection",
+            // we'll make the map data here
+            features: [
+              {
+                type: "Feature",
+                geometry: {
+                  type: "Point",
+                  coordinates: [_streetview.app.svCoords.lng, _streetview.app.svCoords.lat],
+                },
+                properties: {
+                  bearing: _streetview.app.svBearing - 90,
+                },
+              },
+            ],
+        });
     }
 
-    setBearing(_streetview, node, mly, start, end) {
-        if (!node.fullPano) {
-          // We are only interested in setting the bearing for full 360 panoramas.
-          return;
-        }
-        let nodeBearing = node.computedCA; // Computed node compass angle (equivalent
-        // to bearing) is used by mjs when placing
-        // the node in 3D space.
-      
-        // compute this with @turf/bearing
-        let desiredBearing = bearing(start, end); // Your desired bearing.
-        let basicX = _streetview.bearingToBasic(_streetview, desiredBearing, nodeBearing);
-        let basicY = 0.45; // tilt slightly up
-      
-        let center = [basicX, basicY];
-      
-        mly.setCenter(center);
+    loadImagery(_streetview){ 
+        _streetview.setLoading(_streetview, false);
+        _streetview.mly = new Mapillary.Viewer({
+            // Replace this with your own client ID from mapillary.com
+            apiClient: _streetview.mapillaryClientID,
+            component: {
+                cover: false,
+                marker: true,
+                sequence: false,
+                cache: true,
+                attribution: false,
+            },
+            container: 'mly',
+        });
+
+        _streetview.mly.on("nodechanged", (n) => {
+            _streetview.app.svCoords = n.latLon;
+        });
+        
+        _streetview.mly.on("bearingchanged", (b) => {
+            _streetview.app.svBearing = b;
+        });
+
+        _streetview.defaultMarker = new Mapillary.MarkerComponent.SimpleMarker("default-id", { lat: _streetview.app.coords[1], lon: _streetview.app.coords[0] }, _streetview.markerStyle);
+        _streetview.markerComponent = _streetview.mly.getComponent("marker");
+        _streetview.markerComponent.add([_streetview.defaultMarker]);
+
+        _streetview.mly.moveToKey(_streetview.app.currentImageKey.properties.key).then((node) => {
+            console.log('node');
+            console.log(node);
+            _streetview.setBearing(_streetview, node, _streetview.mly, _streetview.app.currentImageKey.geometry.coordinates, [_streetview.app.coords[0], _streetview.app.coords[1]]);
+        });
+        
+        _streetview.app.map.map.loadImage(videoIcon, (error, image) => {
+            if (error) throw error;
+            if(!_streetview.app.map.map.hasImage('video')){
+                _streetview.app.map.map.addImage('video', image);
+            }
+            _streetview.app.map.map.getSource('mapillary').setData({
+              type: "FeatureCollection",
+              // we'll make the map data here
+              features: [
+                {
+                  type: "Feature",
+                  geometry: {
+                    type: "Point",
+                    coordinates: [_streetview.app.svCoords[0], _streetview.app.svCoords[1]],
+                  },
+                  properties: {
+                    bearing: _streetview.app.svBearing - 90,
+                  },
+                },
+              ],
+            });
+        }); 
+    }
+
+    removeImagery(_streetview){
+        let container = document.getElementById('mly');
+        container.removeChild(container.childNodes[1]);
+        container.removeChild(container.childNodes[0]);
+        _streetview.setLoading(_streetview, true);
     }
 
     getImageKey(_streetview){
@@ -221,6 +184,8 @@ export default class Streetview {
             console.log(sorted);
             _streetview.app.imageKeys = sorted;
             _streetview.app.currentImageKey = sorted[sorted.length - 1];
+            _streetview.app.svCoords = _streetview.app.currentImageKey.geometry.coordinates;
+            _streetview.setCameraIcon(_streetview);
             _streetview.loadDateOptions(_streetview);
             _streetview.loadImagery(_streetview);
         }).catch( err => {
@@ -233,6 +198,9 @@ export default class Streetview {
             console.log(key);
             let tempDate = new Date(key.properties.captured_at);
             let tempOption = document.createElement('option');
+            if(_streetview.app.currentImageKey.properties.captured_at == key.properties.captured_at){
+                tempOption.selected = true;
+            }
             tempOption.label = `${tempDate.toLocaleString('default', { month: 'short' })} ${tempDate.getDate()}, ${tempDate.getFullYear()}`;
             tempOption.value = key.properties.captured_at;
             _streetview.dateSelect.appendChild(tempOption);
@@ -241,5 +209,74 @@ export default class Streetview {
 
     buildDate(date){
         return new Date(date);
+    }
+
+    setLoading(_streetview, status){
+        if(status){
+            document.querySelector('.streetview-loader').className = 'streetview-loader active';
+            _streetview.loading = true;
+        }else{
+            document.querySelector('.streetview-loader.active').className = 'streetview-loader';
+            _streetview.loading = false;
+        }
+    }
+
+    changingImageDate(ev, _streetview){
+        console.log('changing date for imagery');
+        console.log(ev);
+        _streetview.app.currentImageKey = _streetview.app.imageKeys.filter((s) => s.properties.captured_at === ev.target.value)[0];
+        _streetview.removeImagery(_streetview);
+        _streetview.app.svCoords = _streetview.app.currentImageKey.geometry.coordinates;
+        _streetview.setCameraIcon(_streetview);
+        _streetview.loadImagery(_streetview);
+    }
+
+    wrap(value, min, max) {
+        let interval = max - min;
+      
+        while (value > max || value < min) {
+          if (value > max) {
+            value = value - interval;
+          } else if (value < min) {
+            value = value + interval;
+          }
+        }
+      
+        return value;
+    }
+
+    bearingToBasic(_streetview, desiredBearing, nodeBearing) {
+        // 1. Take difference of desired bearing and node bearing in degrees.
+        // 2. Scale to basic coordinates.
+        // 3. Add 0.5 because node bearing corresponds to the center
+        //    of the image. See
+        //    https://mapillary.github.io/mapillary-js/classes/viewer.html
+        //    for explanation of the basic coordinate system of an image.
+        var basic = (desiredBearing - nodeBearing) / 360 + 0.5;
+      
+        // Wrap to a valid basic coordinate (on the [0, 1] interval).
+        // Needed when difference between desired bearing and node
+        // bearing is more than 180 degrees.
+        return _streetview.wrap(basic, 0, 1);
+    }
+
+    setBearing(_streetview, node, mly, start, end) {
+        console.log(node.computedCA);
+        if (!node.fullPano) {
+          // We are only interested in setting the bearing for full 360 panoramas.
+          return;
+        }
+        let nodeBearing = node.computedCA; // Computed node compass angle (equivalent
+        // to bearing) is used by mjs when placing
+        // the node in 3D space.
+      
+        // compute this with @turf/bearing
+        let desiredBearing = bearing(start, end); // Your desired bearing.
+        let basicX = _streetview.bearingToBasic(_streetview, desiredBearing, nodeBearing);
+        let basicY = 0.45; // tilt slightly up
+      
+        let center = [basicX, basicY];
+      
+        mly.setCenter(center);
     }
 }
